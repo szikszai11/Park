786844adc549eb04afe2f91e48f5e0bd6f079f3f
subiczadrian <71093033+subiczadrian@users.noreply.github.com>
2020-09-22 10:06:48 +0200

    Add files via upload

--- park.py
+++ park.py
@@ -1,10 +1,9 @@
-print("start")
 a = 25
 b = 75
-print("a = 25")
-print("b = 75")
-c = a * b 
-print("a*b=1875")
-print("c=1875")
-print(c+c+c*0.5)
-print("stop")
+c = a * b
+d = 2.5*c
+
+print("a: %d m" % a)
+print("b: %d m" % b)
+
+print("Gyeptégla: %.f db" % d)
